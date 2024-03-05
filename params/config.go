0e3fb257d6b609168b6c81c1cf2c59d9ba926f38
resilience-me <emailsareobsolete@gmail.com>
2024-03-06 00:52:03 +0100

    Update config.go

--- params/config.go
+++ params/config.go
@@ -113,9 +113,9 @@ var (
 			Period: 15,
 			Epoch:  30000,
 			ValidatorContract: common.Address{},
+			HashOnionFilePath: "./hashOnion.json",
 			GetValidatorCallCode: []byte{0xb5, 0xd8, 0x96, 0x27},
 			ValidatorHashCallCode: []byte{0x63, 0x25, 0xba, 0x74},
-			HashOnionFilePath: "./hashOnion.json",
 			Deadline: 15,
 		},
 	}
