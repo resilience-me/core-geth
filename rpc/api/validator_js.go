dcd5383c0fd05353438279d08e7c83ba751e09a6
resilience-me <emailsareobsolete@gmail.com>
2024-03-23 20:07:35 +0100

    Update and rename miner_js.go to validator_js.go

--- rpc/api/validator_js.go
+++ rpc/api/validator_js.go
@@ -16,68 +16,39 @@
 
 package api
 
-const Miner_JS = `
+const Validator_JS = `
 web3._extend({
-	property: 'miner',
+	property: 'validator',
 	methods:
 	[
 		new web3._extend.Method({
 			name: 'start',
-			call: 'miner_start',
+			call: 'validator_start',
 			params: 1,
 			inputFormatter: [null]
 		}),
 		new web3._extend.Method({
 			name: 'stop',
-			call: 'miner_stop',
+			call: 'validator_stop',
 			params: 1,
 			inputFormatter: [null]
 		}),
 		new web3._extend.Method({
 			name: 'setEtherbase',
-			call: 'miner_setEtherbase',
+			call: 'validator_setEtherbase',
 			params: 1,
 			inputFormatter: [web3._extend.formatters.formatInputInt],
 			outputFormatter: web3._extend.formatters.formatOutputBool
-		}),
-		new web3._extend.Method({
-			name: 'setExtra',
-			call: 'miner_setExtra',
-			params: 1,
-			inputFormatter: [null]
 		}),
 		new web3._extend.Method({
 			name: 'setGasPrice',
 			call: 'miner_setGasPrice',
 			params: 1,
 			inputFormatter: [web3._extend.utils.fromDecial]
-		}),
-		new web3._extend.Method({
-			name: 'startAutoDAG',
-			call: 'miner_startAutoDAG',
-			params: 0,
-			inputFormatter: []
-		}),
-		new web3._extend.Method({
-			name: 'stopAutoDAG',
-			call: 'miner_stopAutoDAG',
-			params: 0,
-			inputFormatter: []
-		}),
-		new web3._extend.Method({
-			name: 'makeDAG',
-			call: 'miner_makeDAG',
-			params: 1,
-			inputFormatter: [web3._extend.formatters.inputDefaultBlockNumberFormatter]
 		})
 	],
 	properties:
 	[
-		new web3._extend.Property({
-			name: 'hashrate',
-			getter: 'miner_hashrate',
-			outputFormatter: web3._extend.utils.toDecimal
-		})
 	]
 });
 `
