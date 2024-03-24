70fda1cd791bca47a6fc5af20c3dc756bf900eac
resilience-me <emailsareobsolete@gmail.com>
2024-03-24 09:40:07 +0100

    Create Election.sol

---
+++ panarchy/Election.sol
@@ -0,0 +1,27 @@
+contract Election {
+
+    Schedule schedule = Schedule(0x0000000000000000000000000000000000000000);
+    BitPeople bitPeople = BitPeople(0x0000000000000000000000000000000000000004);
+
+    mapping (uint => address[]) election;
+
+    mapping (uint => mapping (address => bool)) suffrageToken;
+
+    mapping (uint => mapping (address => uint)) public balanceOf;
+    mapping (uint => mapping (address => mapping (address => uint))) public allowed;
+
+    function vote(address _validator) public {
+        uint t = schedule.schedule();
+        require(balanceOf[t][msg.sender] >= 1);
+        balanceOf[t][msg.sender]--;
+        election[t+2].push(_validator);
+    }
+
+    function allocateSuffrageToken() public {
+        uint t = schedule.schedule();
+        require(bitPeople.proofOfUniqueHuman(t, msg.sender));
+        require(!suffrageToken[t][msg.sender]);
+        balanceOf[t][msg.sender]++;
+        suffrageToken[t][msg.sender] = true;
+    }
+}
