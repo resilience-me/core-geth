2dade251d986db1b756d32b6bd596fa733c8cb35
resilience-me <emailsareobsolete@gmail.com>
2024-03-24 10:41:16 +0100

    Update block_producer.go

--- core/block_producer.go
+++ core/block_producer.go
@@ -485,7 +485,7 @@ func (self *blockProducer) produceBlock(stop <-chan struct{}) {
 
 	if atomic.LoadInt32(&self.mining) == 1 {
 		// commit state root after all state transitions.
-		AccumulateRewards(self.current.state, header)
+		AccumulateRewards(self.coinbase, self.current.state)
 		current.state.SyncObjects()
 		header.Root = current.state.Root()
 	}
