4f7a65ab4e228d50a0805cd4869be46cd10be934
resilience-me <emailsareobsolete@gmail.com>
2024-03-25 00:39:53 +0100

    Update RandomNumberGenerator.sol

--- panarchy/RandomNumberGenerator.sol
+++ panarchy/RandomNumberGenerator.sol
@@ -1,3 +1,11 @@
+// An opt-in random number generator that uses potentially every single person in BitPeople (thus potentially every person in the world) to generate a random number.
+// It uses a commit-reveal scheme, combined with "mutating" the committed value (thus, no one is able to know what they vote for, but they can ensure their vote is random).
+// It here uses BitPeople registered addresses as the "salt" or "seed", for the "mutation". The RNG selects a number between 0 and N, and then selects the address registered 
+// at that index in BitPeople in the previous period (that has been shuffled since the number was committed in this RNG. ) But it could work with any other "seed" as well as
+// simply using the number between 0 and N as the "salt" to the "mutation". The RNG is intended to be used by BitPeople, that will use also use registered addresses as the "seed",
+// and, it can be used by any other application on the ledger as well. It will likely be used by the validators to periodically "reset" their random number generator, making it
+// even more secure.
+
 contract RandomNumberGenerator {
 
     Schedule schedule = Schedule(0x0000000000000000000000000000000000000000);
