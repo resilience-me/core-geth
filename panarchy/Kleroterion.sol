27f0eeb841060a8864ec55c0e8bb957f2f37093d
resilience-me <emailsareobsolete@gmail.com>
2024-03-25 15:13:00 +0100

    Update Kleroterion.sol

--- panarchy/Kleroterion.sol
+++ panarchy/Kleroterion.sol
@@ -11,6 +11,8 @@ contract Kleroterion {
     Schedule schedule = Schedule(0x0000000000000000000000000000000000000000);
     BitPeople bitpeople = BitPeople(0x0000000000000000000000000000000000000005);
 
+    mapping (uint => bytes32) seed;
+
     mapping (uint => mapping (address => bytes32)) commit;
     mapping (uint => uint) votes;
     mapping (uint => mapping (uint => uint)) points;
@@ -19,6 +21,8 @@ contract Kleroterion {
     mapping (uint => mapping (address => bool)) claimed;
     mapping (uint => mapping (address => uint)) public balanceOf;
     mapping (uint => mapping (address => mapping (address => uint))) public allowed;
+
+    function initSeed(uint _t) public { seed[_t] = bitpeople.registry(_t-1, winner[_t]%bitpeople.registered(_t-1)); }
 
     function commitHash(bytes32 _hash) public {
         uint t = schedule.schedule();
@@ -30,8 +34,8 @@ contract Kleroterion {
     function revealHash(bytes32 _preimage) public {
         uint t = schedule.schedule();
         require(schedule.quarter(t) == 2 && keccak256(abi.encode(_preimage)) == commit[t-1][msg.sender]);
-        address seed = bitpeople.registry(t-1, winner[t-1]%bitpeople.registered(t-1));
-        bytes32 mutated = keccak256(abi.encode(_preimage, seed));
+        if(seed[t-1] == 0) initSeed(_t-1);
+        bytes32 mutated = keccak256(abi.encode(_preimage, seed[t-1]));
         uint id = uint(mutated)%votes[t];
         points[t][id]++;
         if (points[t][id] > highscore[t]) {
