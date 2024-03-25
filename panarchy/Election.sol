f79e21a2970136c8ed48640fd0efc55da5231982
resilience-me <emailsareobsolete@gmail.com>
2024-03-25 01:17:32 +0100

    Update Election.sol

--- panarchy/Election.sol
+++ panarchy/Election.sol
@@ -1,7 +1,7 @@
 contract Election {
 
     Schedule schedule = Schedule(0x0000000000000000000000000000000000000000);
-    BitPeople bitPeople = BitPeople(0x0000000000000000000000000000000000000004);
+    BitPeople bitPeople = BitPeople(0x0000000000000000000000000000000000000005);
 
     mapping (uint => address[]) election;
 
