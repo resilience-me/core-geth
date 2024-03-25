6ec5922af1ce264b60f6a2a7c9b97fb48ae733be
resilience-me <emailsareobsolete@gmail.com>
2024-03-25 19:14:45 +0100

    Update Election.sol

--- panarchy/Election.sol
+++ panarchy/Election.sol
@@ -1,4 +1,27 @@
-contract Election {
+contract Token {
+    mapping (uint => mapping (address => uint)) public balanceOf;
+    mapping (uint => mapping (address => mapping (address => uint))) public allowed;
+
+    function _transfer(uint _t, address _from, address _to, uint _value) internal {
+        require(balanceOf[_t][_from] >= _value);
+        balanceOf[_t][_from] -= _value;
+        balanceOf[_t][_to] += _value;
+    }
+    function transfer(address _to, uint _value) external {
+        _transfer(schedule.schedule(), msg.sender, _to, _value);
+    }
+    function approve(address _spender, uint _value) external {
+        allowed[schedule.schedule()][msg.sender][_spender] = _value;
+    }
+    function transferFrom(address _from, address _to, uint _value) external {
+        uint t = schedule.schedule();
+        require(allowed[t][_from][msg.sender] >= _value);
+        _transfer(t, _from, _to, _value);
+        allowed[t][_from][msg.sender] -= _value;
+    }
+}
+
+contract Election is Token {
 
     Schedule schedule = Schedule(0x0000000000000000000000000000000000000000);
     BitPeople bitPeople = BitPeople(0x0000000000000000000000000000000000000005);
@@ -6,9 +29,6 @@ contract Election {
     mapping (uint => address[]) election;
 
     mapping (uint => mapping (address => bool)) suffrageToken;
-
-    mapping (uint => mapping (address => uint)) public balanceOf;
-    mapping (uint => mapping (address => mapping (address => uint))) public allowed;
 
     function vote(address _validator) public {
         uint t = schedule.schedule();
