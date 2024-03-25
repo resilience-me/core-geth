83ca778c5d929e059cccdecc2f521fc02daf7e68
resilience-me <emailsareobsolete@gmail.com>
2024-03-25 02:49:20 +0100

    Update Kleroterion.sol

--- panarchy/Kleroterion.sol
+++ panarchy/Kleroterion.sol
@@ -22,16 +22,14 @@ contract Kleroterion {
 
     function commitHash(bytes32 _hash) public {
         uint t = schedule.schedule();
-        uint deadline = schedule.toSeconds(t)+schedule.period()/2;
-        require(block.timestamp < deadline && balanceOf[t][msg.sender] >= 1);
+        require(!schedule.halftime(t) && balanceOf[t][msg.sender] >= 1);
         balanceOf[t][msg.sender]--;
         commit[t][msg.sender] = _hash;
         votes[t]++;
     }
     function revealHash(bytes32 _preimage) public {
         uint t = schedule.schedule();
-        uint start = schedule.toSeconds(t)+schedule.period()/2;
-        require(block.timestamp >= start && keccak256(abi.encode(_preimage)) == commit[t-1][msg.sender]);
+        require(schedule.halftime(t) && keccak256(abi.encode(_preimage)) == commit[t-1][msg.sender]);
         uint id = winner[t-1];
         address seed = bitpeople.registry[t-1][id%bitpeople.registry[t-1].length];
         bytes32 mutated = keccak256(abi.encode(_preimage, seed));
