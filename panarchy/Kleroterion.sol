88246fea7bc65b437766b16f3c346026cac5cb2e
resilience-me <emailsareobsolete@gmail.com>
2024-03-26 14:19:39 +0100

    Update Kleroterion.sol

--- panarchy/Kleroterion.sol
+++ panarchy/Kleroterion.sol
@@ -1,10 +1,7 @@
 // An opt-in random number generator that uses potentially every single person in BitPeople (thus potentially every person in the world) to generate a random number.
 // It uses a commit-reveal scheme, combined with "mutating" the committed value (thus, no one is able to know what they vote for, but they can ensure their vote is random).
-// It here uses BitPeople registered addresses as the "salt" or "seed", for the "mutation". The RNG selects a number between 0 and N, and then selects the address registered 
-// at that index in BitPeople in the previous period (that has been shuffled since the number was committed in this RNG. ) But it could work with any other "seed" as well as
-// simply using the number between 0 and N as the "salt" to the "mutation". The RNG is intended to be used by BitPeople, that will use also use registered addresses as the "seed",
-// and, it can be used by any other application on the ledger as well. It will likely be used by the validators to periodically "reset" their random number generator, making it
-// even more secure.
+// The RNG selects a number between 0 and N, where N is the number of people who participate. The result conforms to Poisson distribution e^-1/k!. The RNG is intended to be 
+// used by BitPeople, to shuffle the registered population using Fisher-Yates shuffle, and, it can be used by any other application on the ledger as well.
 
 contract Kleroterion is Mixer {
 
