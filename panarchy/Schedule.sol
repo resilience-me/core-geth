fcfe33fba0ae111edae8944464ad74d136038408
resilience-me <emailsareobsolete@gmail.com>
2024-03-27 03:31:53 +0100

    Update Schedule.sol

--- panarchy/Schedule.sol
+++ panarchy/Schedule.sol
@@ -4,6 +4,24 @@ contract Schedule {
     uint constant public period = 4 weeks;
 
     function schedule() public view returns(uint) { return ((block.timestamp - genesis) / period); }
+}
+
+contract Schedule {
+
+    uint constant public genesis = 1710568800;
+    uint constant public period = 4 weeks;
+
+    function schedule() public view returns(uint) { return ((block.timestamp - genesis) / period); }
+    function toSeconds(uint _t) public pure returns (uint) { return genesis + _t * period; }
+    function quarter(uint _t) public view returns (uint) { return (block.timestamp-toSeconds(_t))/(period/4); }
+}
+
+contract Schedule {
+
+    uint constant public genesis = 1710568800;
+    uint constant public period = 4 weeks;
+
+    function schedule() public view returns(uint) { return ((block.timestamp - genesis) / period); }
     function toSeconds(uint _t) public pure returns (uint) { return genesis + _t * period; }
     function quarter(uint _t) public view returns (uint) { return (block.timestamp-toSeconds(_t))/(period/4); }
     function hour(uint _t) public pure returns (uint) { return 1 + uint(keccak256(abi.encode(_t)))%24; }
